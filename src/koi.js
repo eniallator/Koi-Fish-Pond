e6ffe687a4c0a4d6698caa8cc78fa626450b867f
eniallator <eniallatormc@gmail.com>
2022-01-29 18:41:49 +0000

    Did more generalization and now supporting body angles

--- src/koi.js
+++ src/koi.js
@@ -1,4 +1,5 @@
 class Koi {
+  static #bodyRadii = [1, 3 / 4, 1 / 2, 0];
   static #bodySectionCurveDelay = Math.PI / (5 / 2);
   static #bodySectionSpacing = 5;
   static #halfHeadSeparation = 2.5;
@@ -21,7 +22,6 @@ class Koi {
   }
 
   #getBodySegmentPositions() {
-    // Goes from head to the tail
     return new Array(4)
       .fill()
       .map(
@@ -36,35 +36,81 @@ class Koi {
       );
   }
 
+  #rotatePointOnCircumference(center, point, angle) {
+    point.sub(center);
+    const radius = point.getMagnitude();
+    const offset = Vector.UP.multiply(radius);
+    offset.setAngle(point.getAngle() + angle);
+    return offset.add(center);
+  }
+
+  #getBodyAngles(bodyCenters) {
+    return bodyCenters.map((center, i) => {
+      let diff;
+      if (i > 0 && i < bodyCenters.length - 1) {
+        diff = bodyCenters[i - 1]
+          .copy()
+          .sub(center)
+          .add(center.copy().sub(bodyCenters[i + 1]));
+      } else if (i > 0) {
+        diff = bodyCenters[i - 1].copy().sub(center);
+      } else {
+        diff = center.copy().sub(bodyCenters[i + 1]);
+      }
+      return diff.getAngle();
+    });
+  }
+
+  #getBodyPoints(bodyCenters, bodyAngles) {
+    return bodyCenters.map((center, i) => ({
+      right: this.#rotatePointOnCircumference(
+        center,
+        new Vector(
+          center.x + this.#scale * Koi.#halfHeadSeparation * Koi.#bodyRadii[i],
+          center.y
+        ),
+        bodyAngles[i] + Math.PI / 2
+      ),
+      left: this.#rotatePointOnCircumference(
+        center,
+        new Vector(
+          center.x - this.#scale * Koi.#halfHeadSeparation * Koi.#bodyRadii[i],
+          center.y
+        ),
+        bodyAngles[i] - (Math.PI * 3) / 2
+      ),
+    }));
+  }
+
   draw(ctx) {
-    const bodyPos = this.#getBodySegmentPositions();
+    // Body goes from head to the tail
+    const bodyCenters = this.#getBodySegmentPositions();
+    const bodyAngles = this.#getBodyAngles(bodyCenters);
+    const points = this.#getBodyPoints(bodyCenters, bodyAngles);
     ctx.beginPath();
-    ctx.moveTo(
-      bodyPos[0].x + this.#scale * Koi.#halfHeadSeparation,
-      bodyPos[0].y
+    ctx.moveTo(points[0].right.x, points[0].right.y);
+    ctx.bezierCurveTo(
+      points[1].right.x,
+      points[1].right.y,
+      points[2].right.x,
+      points[2].right.y,
+      bodyCenters[3].x,
+      bodyCenters[3].y
     );
     ctx.bezierCurveTo(
-      bodyPos[1].x + this.#scale * Koi.#halfHeadSeparation * (3 / 4),
-      bodyPos[1].y,
-      bodyPos[2].x + this.#scale * Koi.#halfHeadSeparation * (1 / 2),
-      bodyPos[2].y,
-      bodyPos[3].x,
-      bodyPos[3].y
-    );
-    ctx.bezierCurveTo(
-      bodyPos[2].x - this.#scale * Koi.#halfHeadSeparation * (1 / 2),
-      bodyPos[2].y,
-      bodyPos[1].x - this.#scale * Koi.#halfHeadSeparation * (3 / 4),
-      bodyPos[1].y,
-      bodyPos[0].x - this.#scale * Koi.#halfHeadSeparation,
-      bodyPos[0].y
+      points[2].left.x,
+      points[2].left.y,
+      points[1].left.x,
+      points[1].left.y,
+      points[0].left.x,
+      points[0].left.y
     );
     ctx.arc(
-      bodyPos[0].x,
-      bodyPos[0].y,
+      bodyCenters[0].x,
+      bodyCenters[0].y,
       Koi.#halfHeadSeparation * this.#scale,
-      Math.PI,
-      0
+      bodyAngles[0] - Math.PI / 2,
+      bodyAngles[0] + Math.PI / 2
     );
     ctx.stroke();
   }
